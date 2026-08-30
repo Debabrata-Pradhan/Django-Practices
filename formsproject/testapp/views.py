0907b00d0ffb0ee534a78d93045c5cc0d2148219
Debabrata-Pradhan <debabratapradhan890@gmail.com>
2026-08-30 11:47:02 +0530

    Forms Project

--- formsproject/testapp/views.py
+++ formsproject/testapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from testapp.forms import StudentForm
+
+
+def forms_view(request):
+    Submitted = False
+    name=''
+    if request.method=='POST':
+        form =StudentForm(request.POST)
+        if form.is_valid():
+            print('Form validation success and print data')
+            print('Name:',form.cleaned_data['name'])
+            print('RollNo.:',form.cleaned_data['rollno'])
+            print('Marks:',form.cleaned_data['marks'])
+            Submitted=True
+            name=form.cleaned_data['name']
+    form = StudentForm()
+    return render(request,'testapp/index.html',{'form':form,'Submitted':Submitted,'name':name})
