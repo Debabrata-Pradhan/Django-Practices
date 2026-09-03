6c34454efff675fff745840386db376622956593
Debabrata-Pradhan <debabratapradhan890@gmail.com>
2026-09-03 20:21:06 +0530

    Adding manual validation

--- feedbackproject/testapp/forms.py
+++ feedbackproject/testapp/forms.py
@@ -4,3 +4,23 @@ class FeedbackForm(forms.Form):
     rollno=forms.IntegerField()
     email=forms.EmailField()
     feedback=forms.CharField(widget=forms.Textarea)
+    def clean_name(self):
+        print("Validating name field...")
+        inputname=self.cleaned_data['name']
+        if len(inputname)<4:
+            raise forms.ValidationError("The minimum number of character for the name should be 4")
+        return inputname
+    def clean_rollno(self):
+        print("Validating Rollno field...")
+        inputrollno=self.cleaned_data['rollno']
+        if inputrollno<0:
+            raise forms.ValidationError("Roll no should be above 0")
+        return inputrollno
+    def clean_email(self):
+        print("Validating Email field...")
+        inputemail=self.cleaned_data['email']
+        return inputemail
+    def clean_feedback(self):
+        print("Validating Feedback field...")
+        inputfeedback=self.cleaned_data['feedback']
+        return inputfeedback
